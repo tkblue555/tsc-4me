6393193a1d28b6eb10be97eadd1fb2dbc7cdfed0
tkblue555 <tkblue@icloud.com>
2020-03-29 18:47:41 -0500

    2 files modified

--- index.ts
+++ index.ts
@@ -1,6 +1,19 @@
 // Import stylesheets
 import './style.css';
 
+function greeter(person) {
+    return "Hello, " + person;
+}
+
+let user = "Jane User";
+
+//document.body.textContent = greeter(user);
+
+//Compiling your code # 
+
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = '<h1>Hello World</h1>';
+//appDiv.innerHTML = '<h1>' + greeter(user) + '</h1>';
+appDiv.innerHTML =  greeter(user) ;
+
+//appDiv.innerHTML = '<h1>Hello World</h1>';
